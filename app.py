5c361490e32d6154ebf1d84356f10c5d70ac07c7
Sebastian Ibarguen <sebas@vana.gt>
2020-04-28 15:53:33 -0600

    Add basic flask app functinality.

---
+++ app.py
@@ -0,0 +1,41 @@
+import time
+
+import flask
+from flask import request, jsonify
+
+from fuzzy_match import FuzzyMatcher
+from errors import InvalidParameters
+
+
+app = flask.Flask(__name__)
+matcher = FuzzyMatcher()
+
+
+
+@app.errorhandler(InvalidParameters)
+def handle_invalid_usage(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
+
+
+@app.route('/', methods=['GET'])
+def match():
+
+    query_params = request.args
+
+    if 'keyword' not in query_params:
+        raise InvalidParameters('<keyword> parameter is required')
+
+
+    keyword = query_params['keyword']
+
+    start = time.perf_counter()
+    suggestions = matcher.match(keyword)
+    end = time.perf_counter()
+
+    response = {'ok': True, 'suggestions': suggestions, 'time[s]': f'{end - start:0.4f}' }
+    return jsonify(response)
+
+
+app.run()
